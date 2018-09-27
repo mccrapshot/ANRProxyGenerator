becc6b393f2dddb208aebe9e3e11fd4a4f77c4bd
mccrapshot <20262509+mccrapshot@users.noreply.github.com>
2018-09-27 01:47:50 -0400

    Still working on filename function

--- ANRProxyGenerator.py
+++ ANRProxyGenerator.py
@@ -5,22 +5,29 @@ import math
 import sys
 import getopt
 import os
+import glob
 
 base_url = "https://netrunnerdb.com/api/2.0/public/deck/"
-root_dir = "C:\\Netrunner\\"
+#root_dir = "C:\\Netrunner\\"
+root_dir = "/Users/USERNAME/Downloads/Netrunner/"
 resize_height = 346
 resize_width = 243
 usage = 'ANRProxyGenerator.py -d <deck id>'
 
 
-def determineFilename(ID)
-	if (ID(:1) == '00' or ID(:1) == '01' or ID(:1) == '03' or ID(:1) == '05' or ID(:1) == '07' or ID(:1) == '09' or ID(:1) == '13' or ID(:1) == '20' or ID(:1) == '22' or ID(:1) == '23' or ID(:1) == '24') :
-		filename = root_dir + ID(:1) + "*\\" + ID(2:4) + "*.jpg"
+def determineFilename(ID) :
+	#if (ID(:1) == '00' or ID(:1) == '01' or ID(:1) == '03' or ID(:1) == '05' or ID(:1) == '07' or ID(:1) == '09' or ID(:1) == '13' or ID(:1) == '20' or ID(:1) == '22' or ID(:1) == '23' or ID(:1) == '24') :
+	if (ID[0:2] == '00' or ID[0:2] == '01' or ID[0:2] == '03' or ID[0:2] == '05' or ID[0:2] == '07' or ID[0:2] == '09' or ID[0:2] == '13' or ID[0:2] == '20' or ID[0:2] == '22' or ID[0:2] == '23' or ID[0:2] == '24') :
+		#filename = glob.glob(root_dir + ID(:1) + "*\\" + ID(2:4) + "*.jpg")
+		filename = glob.glob(root_dir + ID[0:2] + "*/" + ID[2:5] + "*.jpg")
 		return filename
-	elif (ID(:1) == '02' and ID(:1) == '04' and ID(:1) == '06' and ID(:1) == '08' and ID(:1) == '10' and ID(:1) == '11' and ID(:1) == '12' and ID(:1) == '21') :
-		subfolder = str(int(ID(2:4))//20).zfill(2)
-		filename = root_dir + ID(:1) + "*\\" + subfolder + "*\\" + ID(2:4) + "*.jpg"
-		
+	elif (ID[0:2] == '02' and ID[0:2] == '04' and ID[0:2] == '06' and ID[0:2] == '08' and ID[0:2] == '10' and ID[0:2] == '11' and ID[0:2] == '12' and ID[0:2] == '21') :
+		subfolder = str(int(ID[2:5])//20).zfill(2)
+		#filename = glob.glob(root_dir + ID(:1) + "*\\" + subfolder + "*\\" + ID(2:4) + "*.jpg")
+		filename = glob.glob(root_dir + ID[0:2] + "*/" + subfolder + "*/" + ID[2:5] + "*.jpg")
+	else:
+		print ("\nNo Card Found for card ID" + ID + "!")
+
 
 def main(argv):
     deck_id = -1
