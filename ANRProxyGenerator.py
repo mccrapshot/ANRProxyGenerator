6bbc9fdaa33ea6a3a20abe315bf66ff9dbd3d4bd
mccrapshot <20262509+mccrapshot@users.noreply.github.com>
2018-09-29 22:59:39 -0400

    Resolution improved, needs dimensioning
    Now works in high-res. Also, added commented out lines to switch between Mac and Windows. Need to change to using a flag. Also, need to add proper debugging print statements instead of my commented out crap.

--- ANRProxyGenerator.py
+++ ANRProxyGenerator.py
@@ -8,24 +8,27 @@ import os
 import glob
 
 base_url = "https://netrunnerdb.com/api/2.0/public/deck/"
-#root_dir = "C:\\Netrunner\\"
-root_dir = "/Users/USERNAME/Downloads/Netrunner/"
-resize_height = 346
-resize_width = 243
+root_dir = "C:\\Netrunner\\"
+#root_dir = "/Users/USERNAME/Downloads/Netrunner/"
+resize_height = 2000
+resize_width = 1434
 usage = 'ANRProxyGenerator.py -d <deck id>'
 
 
 def determineFilename(ID) : #get the filename for a card
-    #if (ID(:1) == '00' or ID(:1) == '01' or ID(:1) == '03' or ID(:1) == '05' or ID(:1) == '07' or ID(:1) == '09' or ID(:1) == '13' or ID(:1) == '20' or ID(:1) == '22' or ID(:1) == '23' or ID(:1) == '24') :
     if (ID[0:2] == '00' or ID[0:2] == '01' or ID[0:2] == '03' or ID[0:2] == '05' or ID[0:2] == '07' or ID[0:2] == '09' or ID[0:2] == '13' or ID[0:2] == '20' or ID[0:2] == '22' or ID[0:2] == '23' or ID[0:2] == '24') :
-        #filename = glob.glob(root_dir + ID(:1) + "*\\" + ID(2:4) + "*.jpg")
-        filename = glob.glob(root_dir + ID[0:2] + "*/" + ID[2:5] + "*.jpg")
+        filename = glob.glob(root_dir + ID[0:2] + "*\\" + ID[2:5] + "*.jpg")
+        #print(ID[0:2])
+        #filename = glob.glob(root_dir + ID[0:2] + "*/" + ID[2:5] + "*.jpg")
         return filename[0]
     elif (ID[0:2] == '02' or ID[0:2] == '04' or ID[0:2] == '06' or ID[0:2] == '08' or ID[0:2] == '10' or ID[0:2] == '11' or ID[0:2] == '12' or ID[0:2] == '21') :
         subfolder = str(int(ID[2:5])//20 + 1).zfill(2)
         #print(subfolder)
-        #filename = glob.glob(root_dir + ID(:1) + "*\\" + subfolder + "*\\" + ID(2:4) + "*.jpg")
-        filename = glob.glob(root_dir + ID[0:2] + "*/" + subfolder + "*/" + ID[2:5] + "*.jpg")
+        filename = glob.glob(root_dir + ID[0:2] + "*\\" + subfolder + "*\\" + ID[2:5] + "*.jpg")
+        #print(root_dir + ID[0:2] + "*\\" + subfolder + "*\\" + ID[2:5] + "*.jpg")
+        #print(filename)
+        #print(ID + " " + ID[0:2] + " " + ID[2:5])
+        #filename = glob.glob(root_dir + ID[0:2] + "*/" + subfolder + "*/" + ID[2:5] + "*.jpg")
         return filename[0]
     else:
         print ("No Card Found for card ID: " + ID + "!")
@@ -50,7 +53,7 @@ def main(argv):
                 #card_picture = requests.get("http://netrunnerdb.com/card_image/" + card_id + ".png")
                 card_filename = determineFilename(card_id)
                 #print(card_filename)
-                card_picture = Image.open(card_filename
+                card_picture = Image.open(card_filename)
                 #resized_card_picture = Image.open(BytesIO(card_picture.content)).convert("RGBA")
                 resized_card_picture = card_picture.resize((resize_width, resize_height), Image.LANCZOS)
 
